c6949f04cbfbf1152fb29b665e0abc7595450038
Dhunu <angelsaikia333@gmail.com>
2023-02-11 13:48:11 +0530

    feb 11

--- src/com/angel/usecases/ViewListOfGPM.java
+++ src/com/angel/usecases/ViewListOfGPM.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import com.angel.dao.BDODao;
 import com.angel.dao.BDODaoImpl;
+import com.angel.exceptions.PanchyatMemberExceptions;
 import com.angel.model.PanchayatMember;
-import com.angel.model.Project;
 
 public class ViewListOfGPM {
 	
-	public static void main(String[] args,int blockId) {
+	public static void main(String[] args,int blockId) throws PanchyatMemberExceptions {
 		
 		BDODao bdoDao = new BDODaoImpl();
 		
@@ -18,30 +18,38 @@ public class ViewListOfGPM {
 		if(members.size()==0) {
 			System.out.println("No GPM in database");
 		}else {
-			System.out.println("************************* GPM Table ****************************");
+			System.out.println("***************************************** GPM Table *******************************************");
 			System.out.println();
-			System.out.println("--------------------------------------------------------------------");
-			System.out.println("| Project ID       | Project Name           | Block ID             |");
-			System.out.println("--------------------------------------------------------------------");
+			System.out.println("-----------------------------------------------------------------------------------------------");
+			System.out.println("| GP ID       | GP Name           | GPM Name             | Block ID         | Project ID      |");
+			System.out.println("-----------------------------------------------------------------------------------------------");
 			members.forEach(d ->{
 				System.out.print(
-						"| "+d.getProject_ID());
-				for(int i=0;i<17-String.valueOf(d.getProject_ID()).length();i++) System.out.print(" ");
+						"| "+d.getGP_ID());
+				for(int i=0;i<12-String.valueOf(d.getGP_ID()).length();i++) System.out.print(" ");
 				
 				System.out.print(
-						"| "+d.getProject_Name());
-				for(int i=0;i<23-d.getProject_Name().length();i++) System.out.print(" ");
+						"| "+d.getGP_Name());
+				for(int i=0;i<18-d.getGP_Name().length();i++) System.out.print(" ");
+				
+				System.out.print(
+						"| "+d.getGPM_Name());
+				for(int i=0;i<21-d.getGPM_Name().length();i++) System.out.print(" ");
 				
 				System.out.print(
 						"| "+d.getBlock_ID());
-				for(int i=0;i<21-String.valueOf(d.getBlock_ID()).length();i++) System.out.print(" ");
+				for(int i=0;i<17-String.valueOf(d.getBlock_ID()).length();i++) System.out.print(" ");
+				
+				System.out.print(
+						"| "+d.getProject_ID());
+				for(int i=0;i<16-String.valueOf(d.getProject_ID()).length();i++) System.out.print(" ");
 				
 				System.out.println("|");
 				
 				}
 				
 			);
-			System.out.println("--------------------------------------------------------------------");
+			System.out.println("-----------------------------------------------------------------------------------------------");
 			
 		}
 		
